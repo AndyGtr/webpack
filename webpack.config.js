62039cc4f0a1b8e72127f60c29c39ebf594d5ad0
AndyM <marsden.a@gmail.com>
2023-12-04 12:13:43 +0000

    ignore file, pck json, webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: {
+        // __dirname is a Node.js variable that gives us 
+        // the absolute path to our current directory
         bundle: path.resolve(__dirname, 'src/index.js')},
     output: {
         path: path.resolve(__dirname, 'dist'),
